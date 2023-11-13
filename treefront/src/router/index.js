c10bec9618233c481fced629a326ed9a79169c3d
AhyngK <canon1107@naver.com>
2023-11-13 13:23:02 +0900

    [Created] Project Files

---
+++ treefront/src/router/index.js
@@ -0,0 +1,14 @@
+import { reateRouter, createWebHistory } from 'vue-router'
+c
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+  ]
+})
+
+export default router
