025d7422e98475f6bb1e07de594a6790666e3a35
AhyngK <canon1107@naver.com>
2023-11-15 01:38:23 +0900

    [Inprogress] Home, LoginPage CSS updated

---
+++ treefront/src/components/Home/PostCardView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <img :src=img alt="Post Image">
+        <div class="text-box">
+            <div class="title"> {{ title }} </div>
+            <div class="content"> {{ content }} </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["img","title","content"])
+
+</script>
+
+<style scoped>
+.container{
+    width: 100%; 
+    overflow: hidden; 
+}
+img {
+    width: 100%; 
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    cursor: pointer;
+}
+.text-box{
+    margin-left: 7px;
+    width: 100px;
+    text-align: left;
+}
+.title{
+    font-size: 15px;
+}
+.content{
+    font-size: 14px;
+}
+
+</style>
