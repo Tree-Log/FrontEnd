1e2f3b0274d1b5798b0c8a0e01a79633859c27db
AhyngK <canon1107@naver.com>
2023-11-16 09:11:44 +0900

    [Created] Added Color Codes

--- treefront/src/utils/colorCodeUtil.js
+++ treefront/src/utils/colorCodeUtil.js
@@ -1,1 +1,11 @@
 export const ERROR_COLOR = "#cc0000";
+
+
+export const DARK_GREEN_MAIN = "#5B6150";
+export const MIDDLE_GREEN = "#677145";
+export const LIGHT_GREEN = "#939E7F";
+export const DARK_CREAM = "#B7A88A";
+export const LIGHT_CREAM_MAIN = "#F6EED9";
+export const LIGHT_PINK_MAIN = "#E08F86";
+export const MIDDLE_PINK = "#C9726B";
+export const DARK_PINK = "#B0534C";
