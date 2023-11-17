25e7a1d0ae572521acc2ec70a29c871990304730
AhyngK <canon1107@naver.com>
2023-11-17 21:16:20 +0900

    [Created] React Project file

---
+++ treefront/src/components/board/BoardInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="title">
+            제목
+        </div>
+        <input class="content" type="text">
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+<style lang="scss" scoped>
+
+</style>
